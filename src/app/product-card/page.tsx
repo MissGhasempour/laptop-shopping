996370a02e4473718a2f10ec11ab2eb0de4c773e
MissGhasempour <romi.ghasem@gmail.com>
2025-04-13 12:51:54 +0330

    some bugs fixed

---
+++ src/app/product-card/page.tsx
@@ -0,0 +1,11 @@
+import getProducts from "@/lib/api/getProducts";
+import ProductDefinition from "@/components/product-detail/productDetail";
+
+export default async function Page() {
+  const res = await getProducts();
+  return (
+    <div>
+      <ProductDefinition res={res} />
+    </div>
+  );
+}
