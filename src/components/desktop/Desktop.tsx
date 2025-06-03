ccc520086c7b3900f8e9cfb40eb272185b39c4c7
MissGhasempour <romi.ghasem@gmail.com>
2025-06-03 20:37:09 +0330

    about us & contact us pages created but not complete

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -1,5 +1,24 @@
 import style from "@/css/bg-image.module.css";
+import Image from "next/image";
+import img from '@/images/about-us-page-examples.png'
 
 export default function Desktop() {
-  return <div id={style.bgImage} className="h-250 bg-no-repeat"></div>;
+  return (
+   <div className="bg-sky-500 text-stone-100">
+    <h1 className="font-Serif">About Us</h1>
+     <div  className="grid grid-flow-col grid-rows-3 h-250 bg-no-repeat">
+      
+      <p className="font-Arial row-span-3 w-100">
+        SoftTech aims to empower your business to unleash ecommerce growth by
+        outsmarting risk. Ecommerce fraud teams play a crucial role in enabling
+        their company’s growth and profitability. To do so, you require an
+        enterprise-grade fraud and risk intelligence solution that can
+        efficiently combat fraud, curb policy abuse, and boost revenue to the
+        max. The problem is, the speed, scale, and sophistication of fraud and
+        abuse can stretch the team and profit margins thin.
+      </p>
+      <Image src={img} alt="about us" width={500} height={300} />
+    </div>
+   </div>
+  );
 }
